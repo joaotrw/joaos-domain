fa2bbe2767b4cc4f7953a56859adea3c98f81662
joaotrw <joao.@therealworld.ag>
2026-02-15 11:08:36 -0500

    push

---
+++ pull.bat
@@ -0,0 +1,7 @@
+# Navigate to your repository folder
+cd C:\Users\joao\joaos-domain
+
+# Pull the latest changes from GitHub
+git pull origin main
+# Or if your branch is 'master':
+git pull origin master
